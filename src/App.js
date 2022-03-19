22c7e031731449070db04089f6a8ef033632d5d0
raihan <raihanulmcse@mail.com>
2022-03-20 03:53:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<LoadCountries></LoadCountries>
+		</div>
+	);
 }
 
+// Make a component here
+function LoadCountries() {
+	// 1.step declaring a state
+	const [countries, setCountries] = useState([]);
+	// 2. create a useeffect
+	useEffect(() => {
+		// 3. fetch the data inside useeffect
+		fetch("https://restcountries.com/v3.1/all")
+			.then((res) => res.json())
+			.then((data) => setCountries(data));
+	}, []);
+
+	return (
+		<div>
+			<h1>Visiting every countries in the world</h1>
+			{/* the numbers of data loaded  */}
+			<h3>Available Countries:{countries.length}</h3>
+		</div>
+	);
+}
 export default App;
